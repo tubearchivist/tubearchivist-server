69afaf1fd14171505be355009b8b96281ef31be0
Simon <simobilleter@gmail.com>
2023-08-05 10:48:14 +0700

    add tubearchivist-jf to HOOK_URL

--- tubearchivist/web/src/webhook_github.py
+++ tubearchivist/web/src/webhook_github.py
@@ -19,6 +19,7 @@ HOOK_URL = {
     "tubearchivist/browser-extension": environ.get("GITHUB_COMPANION_HOOK_URL"),
     "tubearchivist/tubearchivist": environ.get("GITHUB_TA_HOOK_URL"),
     "tubearchivist/docs": environ.get("GITHUB_DOCS_URL"),
+    "tubearchivist/tubearchivist-jf": environ.get()
 }
 
 
